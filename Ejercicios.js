9648c42e69e97b50d0a919726b7b18b8a7ebc2df
anitamartinez0 <anapm1712@gmail.com>
2022-09-01 19:05:05 -0300

    listo

--- Ejercicios.js
+++ Ejercicios.js
@@ -1,11 +1,19 @@
-function convertirTexto() {
+
     //Crear una funcion para generar el hash de un texto como numero entero
     //Usando métodos ya establecidos
     //Aqui tu codigo
-}
+    function convertirTexto(texto) {
 
+        if(!texto.length){
+            return null
+        }
+        var text = texto.split('')
+        return text.reduce((h, c) => (c.charCodeAt(0) + h))
+    }
+    
+    console.log(convertirTexto("Joaquin"))
+         
 
-function HashTable() {
     //      Hash Table
     //      Una hash table contiene un arreglo de "contenedores" donde puede guardar información.
     //      Para guardar un valor asociado a una key (string):
@@ -13,5 +21,23 @@ function HashTable() {
     //    - Establecer el par llave/valor en tu tabla hash -- Usa un set
     //    - Devuelve el valor almacenado en la tabla[índice] -- Usa un get
     //      Aqui tu codigo
+    
+    function HashTable() {
 
-}
+        hash(key){
+            return key.reduce((h, c) => (c.charCodeAt(0)+h))
+            suma+=key.charCodeAt(i)
+        }
+        
+        set(key, value) {
+        const index = this.hash(key)
+        this.table[index] = [key, value]
+        this.size
+        }
+        
+        get(key){
+            const target = this.hash(key)
+            return this.table[target]
+        }
+
+    }
